9c3e301d9b2f2cc38e66fe10cb4c667ae117b485
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-31 14:18:08 +0530

    push

--- src/instance.js
+++ src/instance.js
@@ -3,6 +3,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 export default axios.create({
   withCredentials: true,
+  credentials: 'include',
   baseURL: SERVER_URL,
   headers: {
       'Content-Type': 'application/json',
