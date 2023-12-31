fd4f2cb3853dcfa72f886bb7a5cc5136964a4230
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-31 14:05:14 +0530

    pus

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://taskapi-n65y.onrender.com',
+    },
+  },
+
+  routes: [
+    { path: '/', component: '/src/components/Signin/Signin.jsx' },
+    { path: '/login', component: '/src/components/Login/Login.jsx' },
+  ],
 })
