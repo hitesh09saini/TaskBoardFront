249f1020b28062f3b0b9107a2c97e34defd4284e
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-31 12:17:55 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import instance from './instance';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './components/Signin/Signin';
 import Login from './components/Login/Login';
-import Main from './components/Main/Main'
-import instance from './instance';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import Loader from './components/Loader/Loader'
+import Main from './components/Main/Main';
+import Loader from './components/Loader/Loader';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [loader, setLoader] = useState(false);
 
-  const [isLoggedIn, setLoggedIn] = useState(false)
-  const [loader, setLoader] = useState(false)
   const setLoading = (val) => {
-     setLoader(val);
-  }
+    setLoader(val);
+  };
 
   const handleLogin = async () => {
     setLoading(true);
@@ -26,52 +26,44 @@ function App() {
       const responseData = res.data;
       setLoggedIn(true);
     } catch (error) {
-      console.error('Error during login:', error);
-  
-      // Set the logged-in state to false
+      console.error('Error during login:', error.message);
       setLoggedIn(false);
     } finally {
-      // Regardless of success or failure, set loading to false
       setLoading(false);
     }
   };
-  
 
   useEffect(() => {
-    handleLogin().catch(error => {
-      console.error('Login failed:', error);
-    });
+    const login = async () => {
+      setLoading(true);
+      try {
+        await handleLogin();
+      } catch (error) {
+        console.error('Login failed:', error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    login();
   }, []);
-
 
   return (
     <React.StrictMode>
       <DndProvider backend={HTML5Backend}>
         <Router>
           <div className='relative'>
+            <Header isLoggedIn={isLoggedIn} setLoggedIn={handleLogin} />
+            {!isLoggedIn ? (
+              <Routes>
+                <Route path="/" element={<Signin loader={setLoading} login={handleLogin} />} />
+                <Route path="/login" element={<Login loader={setLoading} login={handleLogin} />} />
+              </Routes>
+            ) : (
+              <Main loader={setLoading} />
+            )}
 
-            <Header Login={isLoggedIn} setLoggedIn={handleLogin} />
-            {!isLoggedIn ?
-              (
-                <Routes>
-                  <Route path="/" element={<Signin loader={setLoading} login={handleLogin} />} />
-                  <Route path="/login" element={<Login loader={setLoading} login={handleLogin} />} />
-                </Routes>
-              ) : (
-                <Main loader={setLoading} />
-              )
-            }
-
-            < Footer />
-
-            {
-              loader ? (
-                <Loader />
-              ) : (
-                ""
-              )
-            }
-
+            <Footer />
+            {loader && <Loader />}
           </div>
         </Router>
       </DndProvider>
