dcf9ec96c02f6fec476465fa118898089f0377ee
Hitesh <hitesh9694saini@gmail.com>
2024-04-12 02:45:38 +0530

    add loader design

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
-import './loader.css'; 
+import './loader.css';
 
 const Loader = () => {
   return (
-    <div className="loader-container">
-      <div className="loader"></div>
+    <div className='absolute top-0 left-0 w-screen h-screen bg-gray-800  flex justify-center items-center'>
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
     </div>
   );
 };
